95a8c5cce50bd491c5e08caa458f9f1e4a6c6be9
Vladimir Beliaev <vladbel@live.com>
2016-08-05 16:05:58 -0500

    RP.ViewModels.DesignTime

---
+++ RP.ViewModels.DesignTime/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using RP.ViewModels.Common;
+
+namespace RP.ViewModels.DesignTime
+{
+    public class MainPageViewModel: BaseViewModel
+    {
+        public MainPageViewModel()
+        {
+            Property = "Designe Time Property";
+        }
+
+        private string _property;
+        public string Property
+        {
+            get
+            {
+                return _property;
+            }
+            set
+            {
+                _property = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
