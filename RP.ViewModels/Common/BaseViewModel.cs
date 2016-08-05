d3858f41c082c1229b04934afdd19c2e4d08c2d5
Vladimir Beliaev <vladbel@live.com>
2016-08-05 15:31:30 -0500

    RP: initial implementation

---
+++ RP.ViewModels/Common/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RP.ViewModels.Common
+{
+    public abstract class BaseViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
---
+++ RP.ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RP.ViewModels.Common;
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+namespace RP.ViewModels
+{
+    public class MainPageViewModel: BaseViewModel
+    {
+        public MainPageViewModel()
+        {
+            Action = new RelayCommand(
+                async() =>
+                {
+                    await ActionAsync();
+                }
+                );
+
+        }
+
+        private string _property;
+        public string Property
+        {
+            get
+            {
+                return _property;
+            }
+            set
+            {
+                _property = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand Action { get; set; }
+
+        private async Task ActionAsync()
+        {
+            Property = "aaa";
+            await Task.Delay(1000);
+            Property = "bbb";
+        }
+    }
+}
