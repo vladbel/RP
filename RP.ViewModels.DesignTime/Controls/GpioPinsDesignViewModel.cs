7d2155c346f02f2fb5263f6faa457aa4e1de7bd2
Vladimir Beliaev <vladbel@live.com>
2016-08-18 17:28:03 -0500

    RP: Pins control initial implementation

---
+++ RP.ViewModels.DesignTime/Controls/GpioPinsDesignViewModel.cs
@@ -0,0 +1,26 @@
+﻿using RP.ViewModels.Controls;
+using System.Collections.ObjectModel;
+
+
+namespace RP.ViewModels.DesignTime.Controls
+{
+    public class GpioPinsDesignViewModel: GpioPinsViewModel
+    {
+        public GpioPinsDesignViewModel()
+        {
+            Pins = new ObservableCollection<PinViewModel>()
+            {
+                new PinViewModel()
+                {
+                    PinValue = 55,
+                    PinMode = PinMode.Read
+                },
+                new PinViewModel()
+                {
+                    PinValue = 66,
+                    PinMode = PinMode.Write
+                }
+            };
+        }
+    }
+}
---
+++ RP.ViewModels/Controls/GpioPinsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using RP.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP.ViewModels.Controls
+{
+    public class GpioPinsViewModel : BaseViewModel
+    {
+        private ObservableCollection<PinViewModel> _pins;
+
+        public ObservableCollection<PinViewModel> Pins
+        {
+            get
+            {
+                return _pins;
+            }
+            set
+            {
+                _pins = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
